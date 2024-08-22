b23c4386ba9f3d2096d091ce43d4b51deb0649aa
Javier Artola <javier.artola@iter.org>
2024-08-22 13:34:40 +0200

    add scripts to load to IMAS DB

--- IMAS/plot_Te_profile.py
+++ IMAS/plot_Te_profile.py
@@ -1,11 +1,11 @@
 import imas
 import matplotlib.pyplot as plt
 
-database = 'test_database'
-user     = 'artolaj'
-shot     = 111111
+database = 'ITER'
+user     = 'public'
+shot     = 105033
 run      = 1
-t_index  = 0
+t_index  = 56
 
 imas_entry = imas.DBEntry(imas.imasdef.MDSPLUS_BACKEND, database, shot, run, user, data_version = '3')
 imas_entry.open()
@@ -15,7 +15,7 @@ core_profiles = imas_entry.get('core_profiles')
 rho = core_profiles.profiles_1d[t_index].grid.rho_tor_norm
 Te  = core_profiles.profiles_1d[t_index].electrons.temperature
 
-plt.xlabel(r'$\rho$ [m]')
+plt.xlabel(r'$\rho_{norm}$')
 plt.ylabel(r'$T_e$')
 plt.grid()
 plt.plot(rho, Te)
