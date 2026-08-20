e66af3631f6654364a07122c18edd7c5f31d8d50
Javier Artola <javier.artola@iter.org>
2026-08-20 14:54:26 +0200

    add smooth gaussian for initial conditions

--- plasma/1d_fem_solver/initial_conditions.py
+++ plasma/1d_fem_solver/initial_conditions.py
@@ -1,10 +1,22 @@
 import numpy as np
 from constants import *
+from basis import basisfunctions1
+
+
+def f1(x,x0,w):
+    f = np.exp(-((x-x0)/w)**2)
+    f_x = - f * ((x-x0)/w) * 2.0 / w
+    return f, f_x
 
 def initial_conditions(grid, input_params):
-    for node in grid.nodes:
-        x = node.coord[0]
-        node.values[0, 0] = 1.0
-        node.values[0, 1] = 0.0 # Example initial condition for the second DOF
+
+    i_var = 0
+
+    for inode, node in enumerate(grid.nodes):
+        x = node.coord[0] 
+        x_s = node.coord[1]
+        f, f_x = f1(x,2.5,0.8)
+        node.values[i_var, 0] = f
+        node.values[i_var, 1] = f_x * x_s / 3
 
     return grid
